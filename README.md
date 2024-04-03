07f278d6a78b38d83a09777fb785ca0e9d0e19e7
kshitij.k <kshitij.k@osmosys.co>
2024-04-03 23:24:04 +0530

    feat: add transcript-summarizer poc

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # transcript-summarizer
  TranscriptSummarizer is a CLI tool that quickly turns call transcripts into concise meeting minutes, highlighting key points and actions. Ideal for saving time and capturing essential details from any text-based transcript.
+
+ ## Prerequisites
+
+To run this project, you need to have Python version 3.6 or above installed in your system.
+- [Download Python](https://www.python.org/downloads/)
+- [Visual Studio Code](https://code.visualstudio.com/)
+- [OpenAI Account](https://platform.openai.com/api-keys) for API key
+- Transcript from teams meeting
+
+## Setup
+
+1. Use pip to install requirements.txt
+
+```shell
+pip install -r requirements.txt
+```
+
+2. Create a new file `.env`. Copy all data from [env example](./env-example) and paste it in `.env`.
+
+3. Add your API key to `.env`
+```shell
+OPENAI_KEY=sk-your-openai-api-key
+```
+
+## Run
+
+1. Paste your transcript as `your-file-name.txt` in [input folder](./input).
+
+2. Replace the file for which you want meeting minutes in [main function](./main.py).
+
+3. Run the program.
---
+++ main.py
@@ -0,0 +1,171 @@
+import openai
+import os
+import datetime
+from dotenv import load_dotenv
+
+# Load .env file
+load_dotenv()
+
+# Initialize OpenAI API key
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+###-------------Backup functions here-------------###
+
+"""
+# Function to read all data from inputs folder
+# Currently not in use
+
+def read_text_files(input_folder_path):
+    text_data = {}
+    for filename in os.listdir(input_folder_path):
+        if filename.endswith('.txt'):
+            file_path = os.path.join(input_folder_path, filename)
+        with open(file_path, 'r') as file:
+                content = file.read()
+                file.close()
+                text_data[filename] = content.rstrip('\n')
+    return text_data
+"""
+
+"""
+# Helper function to display transcript data
+# Not in use
+
+def display_data_in_transcripts(transcripts_dict):
+    for key in transcripts_dict:
+        print(f"-----------\nkey:")
+        print(key)
+        print(f"=========\nvalue:")
+        print(transcripts_dict.get(key))
+"""
+
+"""
+# Function to get transcript data in chunks from file
+def get_transcript_data(input_folder_path, filename):
+    file_path = os.path.join(input_folder_path, filename)
+    with open(file_path, 'r') as file:
+        data_from_file = file.read()
+        file.close()
+    print(f'Using data from {filename}\n-----------------------\n')
+    return data_from_file
+"""
+
+###-------------Functions declared here-------------###
+
+### Function to chunk file
+def chunk_file(input_folder_path, filename, chunk_size):
+    print(f'\n-----------------------\nUsing data from {filename}\n-----------------------\n')
+    input_file = os.path.join(input_folder_path, filename)
+    with open(input_file, 'r') as file:
+        lines = file.readlines()
+        total_lines = len(lines)
+        num_chunks = total_lines // chunk_size
+        if total_lines % chunk_size != 0:
+            num_chunks += 1
+
+        for i in range(num_chunks):
+            start_index = i * chunk_size
+            end_index = min((i + 1) * chunk_size, total_lines)
+            chunk = lines[start_index:end_index]
+            yield chunk
+
+### function to remove extensions from filename
+def remove_extension(filename):
+    # Split the filename into its base name and extension
+    base_name, extension = os.path.splitext(filename)
+    # Return only the base name
+    return base_name
+
+### Function to create output file
+def create_output_file(output_folder, filename, response):
+    # Create the directory if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Get the current date and time & format as a string
+    current_time = datetime.datetime.now()
+    formatted_time = current_time.strftime("%H%M%S")
+
+    # Create output file
+    outputFileName = f"{formatted_time}_{filename}.txt"
+    file_path = os.path.join(output_folder, outputFileName)
+
+    # write data in new file
+    with open(file_path, "w") as outputFile:
+        outputFile.writelines(response)
+        outputFile.close()
+        print(f"=======================\nResponse saved in {file_path}\n=======================\n")
+
+### Function that generates minutes of meeting using transcript snippets
+def summarize_meeting_using_chatbot(context):
+    # Get the current date and time
+    current_time = datetime.datetime.now()
+
+    # Format the current time as a string
+    formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+
+    message_template =f"""
+      Act as a meeting note taker. I have a transcript of a teams meeting as follows: {context}
+
+      I want a summary of the transcript in the following format:
+
+      # Meeting Minutes
+      Give appropriate discussion heading
+      Created on: {formatted_time}
+      Duration: Set as last recorded response end time and format it as hh:mm:ss
+
+      # Participants
+      Display list of participants involved
+
+      # Agenda
+      Brief summary what the meeting was about in max 3 points
+
+      # Discussion
+      Give the main disucssion points and the speaker names. Give answer in points. Be as descriptive as possible.
+
+      # Action items
+      Give what was the outcome, tasks assigned, end result if any of the discussion in points
+    """
+
+    chat = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": message_template
+            }
+        ]
+    )
+    reply = chat.choices[0].message.content
+    print(f"ChatGPT:\n{reply}")
+    return reply
+
+###-------------Main starts here-------------###
+
+def main():
+    # Initialize input and output folders
+    input_folder_path = os.getenv("INPUT_PATH")
+    output_folder_path = os.getenv("OUPUT_PATH")
+
+    # Get Filename set input folder to create meeting minutes of
+    filename = 'sample-site-creation.txt'
+    no_ext_filename = remove_extension(filename)
+
+    # Get the current date and time & format as a string
+    current_time = datetime.datetime.now()
+    formatted_time = current_time.strftime("%Y%m%d")
+
+    # Variables for assigning chunk data
+    chunk_size = 930
+    output_chunk_folder_path = f"{output_folder_path}//{formatted_time}_{no_ext_filename}_output"
+    output_files_prefix = "_output_chunk_"
+
+    for i, chunk in enumerate(chunk_file(input_folder_path, filename, chunk_size)):
+        print(f"------------- Evaluating chunk {i + 1} -------------")
+        output_file_name = f"{no_ext_filename}{output_files_prefix}{i+1}"
+
+        chatgpt_reply = summarize_meeting_using_chatbot(chunk)
+        create_output_file(output_chunk_folder_path, output_file_name, chatgpt_reply)
+
+if __name__ == "__main__":
+    main()
